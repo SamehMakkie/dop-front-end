04e190d4a2f526f82877102be0581181a4dec29c
Sameh Makkie <44980811+SamehMakkie@users.noreply.github.com>
2022-11-21 01:19:29 +0200

    Add Carousel to Home page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import Carousel from '../views/Carousel/Carousel'
 import NavBar from '../views/NavBar/NavBar'
 
 export default function Home() {
   return (
     <div>
       <NavBar />
+      <Carousel />
     </div>
   )
 }
