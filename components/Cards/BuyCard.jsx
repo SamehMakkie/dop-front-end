b520e0f351717adb110b8477394391893bb31824
Sameh Makkie <44980811+SamehMakkie@users.noreply.github.com>
2022-12-15 22:42:29 +0200

    Show a Toast when the add to cart button is pressed in the game page

--- components/Cards/BuyCard.jsx
+++ components/Cards/BuyCard.jsx
@@ -1,10 +1,15 @@
 import { TiStarFullOutline, TiStarOutline } from "react-icons/ti";
-import { Button, Heading, HStack, Icon, Text, VStack } from "@chakra-ui/react";
+import { Button, Heading, HStack, Icon, Text, useToast, VStack } from "@chakra-ui/react";
 import { useState } from "react";
 
 const BuyCard = () => {
   const [rating, setRating] = useState(0);
   const array = [...Array(5).keys()];
+  const toast = useToast()
+
+  const handleAddToCart = () => {
+    toast({title: "Game added to cart", description: "Game has been succesfully added to the cart.", duration: 1500, isClosable: true, status: 'success', position: "top-right" })
+  }
 
   return (
     <VStack
@@ -30,7 +35,7 @@ const BuyCard = () => {
         ))}
         <Text fontSize={"sm"}>{rating}</Text>
       </HStack>
-      <Button w="100%" colorScheme={"teal"} variant="outline">
+      <Button w="100%" colorScheme={"teal"} variant="outline" onClick={handleAddToCart}>
         Add to Card
       </Button>
       <Button w="100%" colorScheme={"teal"}>
