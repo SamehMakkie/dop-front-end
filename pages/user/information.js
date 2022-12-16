bea7988c02732a84bc19a29938120c3a5f3c75d2
Sameh Makkie <44980811+SamehMakkie@users.noreply.github.com>
2022-12-16 23:41:13 +0200

    Wrap the user profile page with a protected route component

--- pages/user/information.js
+++ pages/user/information.js
@@ -6,6 +6,7 @@ import {
   TabPanels,
   Tabs,
 } from "@chakra-ui/react";
+import ProtectedRoute from "../../components/ProtectedRoute/ProtectedRoute";
 import NavigationWrapper from "../../views/NavigationWrapper/NavigationWrapper";
 import PageStackSpacing from "../../views/PageStackSpacing/PageStackSpacing";
 import Email from "../../views/UserInformationTabs/Email";
@@ -15,27 +16,29 @@ import Profile from "../../views/UserInformationTabs/Profile";
 const information = () => {
   return (
     <NavigationWrapper>
-      <PageStackSpacing>
-        <Heading>Change User Information</Heading>
-        <Tabs w="100%" isFitted colorScheme={"teal"}>
-          <TabList>
-            <Tab>Profile</Tab>
-            <Tab>Email</Tab>
-            <Tab>Password</Tab>
-          </TabList>
-          <TabPanels>
-            <TabPanel>
-              <Profile />
-            </TabPanel>
-            <TabPanel>
-              <Email />
-            </TabPanel>
-            <TabPanel>
-              <Password />
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
-      </PageStackSpacing>
+      <ProtectedRoute>
+        <PageStackSpacing>
+          <Heading>Change User Information</Heading>
+          <Tabs w="100%" isFitted colorScheme={"teal"}>
+            <TabList>
+              <Tab>Profile</Tab>
+              <Tab>Email</Tab>
+              <Tab>Password</Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel>
+                <Profile />
+              </TabPanel>
+              <TabPanel>
+                <Email />
+              </TabPanel>
+              <TabPanel>
+                <Password />
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </PageStackSpacing>
+      </ProtectedRoute>
     </NavigationWrapper>
   );
 };
